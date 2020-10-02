185437c0f168e0542d4a004a6fecf2e5bc8cf0dc
siva1005 <siva.krishna@impactanalytics.co>
2020-10-03 01:40:49 +0530

    deployment config

--- src/constants/routePaths.js
+++ src/constants/routePaths.js
@@ -1,5 +1,7 @@
-const __landing_path = "/";
-const __catalog_path = "/catalog";
-const __catalog_details_path = "/catalog-details";
+const base_url = process.env.PUBLIC_URL;
+
+const __landing_path = base_url + "/";
+const __catalog_path = base_url + "/catalog";
+const __catalog_details_path = base_url + "/catalog-details";
 
 export { __landing_path, __catalog_path, __catalog_details_path };
