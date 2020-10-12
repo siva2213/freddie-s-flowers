e64e931fd157b9d1ee083b3f81039376e56c2234
siva1005 <siva.krishna@impactanalytics.co>
2020-10-12 23:31:53 +0530

    Updates

---
+++ precache-manifest.06c8d53c41ce3ce5c32220e8552b6472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a003da5e392e6e2832a1dea6e3effd",
+    "url": "/freddie-s-flowers/index.html"
+  },
+  {
+    "revision": "bf4c7a0c93e0cb422d4b",
+    "url": "/freddie-s-flowers/static/css/main.6f137576.chunk.css"
+  },
+  {
+    "revision": "cebcea97af5e08a7d0cc",
+    "url": "/freddie-s-flowers/static/js/2.7e63cf24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/freddie-s-flowers/static/js/2.7e63cf24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4c7a0c93e0cb422d4b",
+    "url": "/freddie-s-flowers/static/js/main.1a21f9bd.chunk.js"
+  },
+  {
+    "revision": "61b13d8c16332048aeef",
+    "url": "/freddie-s-flowers/static/js/runtime-main.52360f83.js"
+  }
+]);
